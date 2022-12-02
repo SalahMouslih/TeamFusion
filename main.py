c4de67b10bc111e70cb30d1e1bbaa1d2fcc8f268
salahddin <salah-eddine.elmouslih@ensae.fr>
2022-12-02 20:48:48 +0100

    add post

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 import spacy 
 import json
+from pydantic import BaseModel
+from typing import List
+
+
 
 model = spacy.load('en_core_web_sm')
 
 app = FastAPI()
 
+
+
+class Input(BaseModel):
+    sentence: str
+
+class Extraction(BaseModel):
+    first_index: int
+    last_index: int
+    name: str
+    content: str
+
+class Output(BaseModel):
+    extractions: List[Extraction]
 
 @app.get("/")
 async def root():
@@ -19,3 +36,19 @@ def prediction(text : str):
     for entity in doc.ents:
         entities[entity.text] = entity.label_
     return json.dumps(entities)
+
+
+@app.post("/extractions", response_model=Output)
+def extractions(input: Input):
+    document = model(input.sentence)
+
+    extractions = []
+    for entity in document.ents:
+      extraction = {}
+      extraction["first_index"] = entity.start_char
+      extraction["last_index"] = entity.end_char
+      extraction["name"] = entity.label_
+      extraction["content"] = entity.text
+      extractions.append(extraction)
+
+    return {"extractions": extractions}
