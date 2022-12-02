50e81a3b08b6bb1af96f5c69da0cf5201eaa26bd
salahddin <salah-eddine.elmouslih@ensae.fr>
2022-12-02 20:39:29 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+import spacy 
+import json
+
+model = spacy.load('en_core_web_sm')
 
 app = FastAPI()
 
@@ -6,3 +10,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/spacy/prediction/{text}')
+def prediction(text : str):
+    #db.insert(text)
+    entities : Dict = {}
+    doc = model(text)
+    for entity in doc.ents:
+        entities[entity.text] = entity.label_
+    return json.dumps(entities)
