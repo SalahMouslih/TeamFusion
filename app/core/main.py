598bec6a41885cee89a3901902d918718c196092
Sidibe Boubacar <boubacar.sidibe@ensta-paris.fr>
2022-12-23 17:59:23 +0100

    modif Boubacar

--- app/core/main.py
+++ app/core/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-import spacy 
+import spacy
 import json
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+import rake_nltk
+from rake_nltk import Rake
 from pydantic import BaseModel
 from typing import List
 from pyresparser import ResumeParser
@@ -41,37 +47,24 @@ def upload(file: UploadFile = File(...)):
     try:
         file_location = f"files/{file.filename}"
 
-        contents =  ResumeParser(file_location).get_extracted_data()
+        contents = ResumeParser(file_location).get_extracted_data()
         print(contents)
-        return  {"message": file_location}
+        return {"message": file_location}
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
 
+@app.post("/text_extraction")
+def skills_extractions(input: Input):
+    r = Rake()
+    res = []
 
+    r.extract_keywords_from_text(input.sentence)
+    print()
 
-@app.post('/extract')
-def exctract(text : str):
-    #db.insert(text)
-    entities : Dict = {}
-    doc = model(text)
-    for entity in doc.ents:
-        entities[entity.text] = entity.label_
-    return json.dumps(entities)
+    for rating, keyword in r.get_ranked_phrases_with_scores():
+        if rating > 4:
+            res.append(keyword)
 
-
-@app.post("/extractions", response_model=Output)
-def extractions(input: Input):
-    document = model(input.sentence)
-
-    extractions = []
-    for entity in document.ents:
-      extraction = {}
-      extraction["first_index"] = entity.start_char
-      extraction["last_index"] = entity.end_char
-      extraction["name"] = entity.label_
-      extraction["content"] = entity.text
-      extractions.append(extraction)
-
-    return {"extractions": extractions}
+    return {"Key words": res}
