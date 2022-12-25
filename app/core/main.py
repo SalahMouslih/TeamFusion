0e299d9d93c2fb66b804b6ee22778c2f81212dcb
salahddin <salah-eddine.elmouslih@ensae.fr>
2022-12-25 13:41:20 +0100

    update resume extraction code and UI, added tests

--- app/core/main.py
+++ app/core/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 import spacy 
 import json
 from pydantic import BaseModel
@@ -7,51 +7,120 @@ from pyresparser import ResumeParser
 from fastapi import FastAPI, File, UploadFile, Form
 from nltk.tokenize import word_tokenize
 from typing import Optional
+from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from io import  BytesIO
+from resume_parser import resumeparse
+from tempfile import TemporaryDirectory
+import os
+import aiofiles
+from app.core import database
+from starlette.datastructures import URL
+
+
+database.init_db()
+
+
 
 model = spacy.load('en_core_web_sm')
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 
 class Input(BaseModel):
     sentence: str
 
-class Extraction(BaseModel):
-    first_index: int
-    last_index: int
-    name: str
-    content: str
+class Resume(BaseModel):
+    name : str
+    email : str
+    phone : str
+    degree : str
+    skills : List
+    tot_exp : int
 
-class Output(BaseModel):
-    extractions: List[Extraction]
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return RedirectResponse(url='/home')
 
 
-@app.get("/hello/{name}")
-async def root(name:str):
-    return {"noms": name}
+@app.get("/home", response_class = HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.post("/upload/")
-def upload(file: UploadFile = File(...)):
+@app.get("/uploadresume",response_class=HTMLResponse )
+async def form_post(request: Request, msg: Optional[str] = None,):
+    return templates.TemplateResponse("add_resume.html", {"request": request, "msg": msg})
+
+@app.post("/uploadresumes")
+def upload(request:Request, file: UploadFile = File(...)):
     try:
-        file_location = f"files/{file.filename}"
+        skills = []
+        f = open('files/skills.txt', 'r')
+        skills = f.read().split('\n')
 
-        contents =  ResumeParser(file_location).get_extracted_data()
-        print(contents)
-        return  {"message": file_location}
+        #create temp directory
+        with TemporaryDirectory() as tmp_dir:              
+            file_path = os.path.join(tmp_dir,'resume.pdf')
+            #write file to temp_dir
+            with open(file_path, "wb") as f:
+                f.write(file.file.read())
+                
+                #extract resume infos
+                #data = ResumeParser(file_path).get_extracted_data()
+                data = resumeparse.read_file(file_path)
+                
+
+                resume = {}
+                resume["name"] = data["name"]
+                resume["email"] = data["email"]
+                resume["phone"] = data["phone"]
+                resume["degree"] = data["degree"]
+                resume["skills"] = []
+                resume["skills"] = [skill for skill in data["skills"] if skill.lower() in skills]
+                print(resume["skills"])    
+                resume["tot_exp"] = data["total_exp"]
+                database.insert(resume)
+                
+                message = True
+                redirect_url = URL(request.url_for('form_post')).include_query_params(msg=message)
+                return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+
+
     except Exception:
-        return {"message": "There was an error uploading the file"}
+            message = False
+            redirect_url = URL(request.url_for('form_post')).include_query_params(msg=message)
+            return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+
+
     finally:
         file.file.close()
 
 
 
-@app.post('/extract')
+
+@app.get("/resumes")
+async def list(request:Request):
+    res = database.list()
+    resumes = []
+    for data in res:
+        resume = {}
+        resume["name"] = data[1]
+        resume["email"] = data[2]
+        resume["phone"] = data[3]
+        resume["skills"] = data[4]
+        resumes.append(resume)
+
+    return templates.TemplateResponse("list_resumes.html", {"request": request, "resumes": resumes})
+
+
+@app.get('/extract/{text}')
 def exctract(text : str):
     #db.insert(text)
     entities : Dict = {}
@@ -60,7 +129,7 @@ def exctract(text : str):
         entities[entity.text] = entity.label_
     return json.dumps(entities)
 
-
+'''
 @app.post("/extractions", response_model=Output)
 def extractions(input: Input):
     document = model(input.sentence)
@@ -75,3 +144,4 @@ def extractions(input: Input):
       extractions.append(extraction)
 
     return {"extractions": extractions}
+'''
---
+++ tests/test_app.py
@@ -0,0 +1,61 @@
+import pytest
+import json
+from fastapi.testclient import TestClient
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+from app.core import main 
+
+client = TestClient(main)
+
+def test_upload(client):
+
+    '''
+    filename = "files/resume_01.pdf"
+    
+    file = MultipartEncoder(
+        fields={'file': ('filename', open(filename, 'rb'), 'application/pdf')}
+        )
+
+    response = client.post("/uploadresume",
+                           data=file,
+                           headers={"Content-Type": "multipart/form-data"}
+                           )
+
+    assert response.status_code == 200
+    test_file = "files/resume_01.pdf"
+    files = {'file': ("resume_01.pdf", open(test_file, 'rb'))}
+    response = client.post('/app/core/main/uploadresume', files=files)
+    print(test_file)
+    print(response.json())
+        '''
+
+    with test_file.open('rb') as f:
+        files = {'files': f}
+        response = client.post('/uploadresume',
+                            files=files)
+    assert 0
+    #assert response.status_code == 303
+    #assert response.json() == {"message": True} or response.json() == {"message": False}
+
+
+
+'''
+def test_prediction_json(client):
+    res = client.post('/uploadresume',
+                      json={'text': "When Sebastian Thrun started working on self-driving cars \
+                          at Google in 2007, few people outside of the company took him seriously. \
+                              I can tell you very senior CEOs of major American car companies would \
+                                  shake my hand and turn away because I wasn’t worth talking to, said \
+                                      Thrun, in an interview with Recode earlier this week.)"}) 
+                                      # Send a POST request on the route /spacy/prediction
+    data = json.loads(res.data) # Convert binary result res.data to Dict
+
+    expected_result = {
+        '2007': 'DATE',
+        'American': 'NORP',
+        'Recode': 'ORG',
+        'Sebastian Thrun': 'PERSON',
+        'earlier this week': 'DATE',
+    }
+
+    assert data == expected_result
+'''
