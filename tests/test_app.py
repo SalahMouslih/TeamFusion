30d2de4ceae48c2c709784d6c3f8c87f2fec54dc
salahddin <salah-eddine.elmouslih@ensae.fr>
2022-12-28 14:06:04 +0100

    add unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,50 +3,60 @@ import json
 from fastapi.testclient import TestClient
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 from app.core.main import app 
+from app.core import database 
+
 
 @pytest.fixture 
 def client():
     with TestClient(app) as client:
         yield client
 
+@pytest.fixture
+def test_db():
+    db = database.init_db()
+    yield db
 
 
-def test_upload(client):
+def test_home(client):
+    
+    response = client.get('/home')
+
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == 'text/html; charset=utf-8'
+
+
+
+def test_upload(client,test_db):
 
     filename = "files/resume_01.pdf"
     response = client.post("/uploadresumes",
                             files ={"file": ("filename", open(filename, "rb"), "application/json")}
                         )
-    
-    excpected_result ={
-    "resume": {
-        "name": "John Smith",
-        "email": "email@email.com",
-        "phone": "3868683442",
-        "degree": [],
-        "skills": [
-            "SQL",
-            "Java",
-            "Apache Spark",
-            "Python"
-        ],
-        "tot_exp": 0
-    }
-    }
+    #resume = client.session.get("resume")
     assert response.status_code == 303
-    #assert response.json["resumes"] == excpected_result
     assert response.is_redirect == True
 
 
-'''
-def test_get_resume(client, test_upload):
-    resp = client.post('/uploadresumes', params={"msg": "None"})
-    response = test_upload
-    assert response.text() == 0
+def test_list_resumes(client,test_db):
+    
+    expected_resume = [
+        {
+        'email': 'email@email,com',
+        'name': 'dummy name',
+        'phone': '111111',
+        'skills': 'SQL, Linux, Git'
+        }
+    ]
+    
+    response = client.get("/resumes")
+    
+    assert response.context["resumes"] == expected_resume
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == 'text/html; charset=utf-8'
 
-'''
 
-def test_upload_project(client):
+def test_upload_project(client,test_db):
+    
     data={
         "name" : "dummy project",
         "text" : "dummy text"
@@ -57,16 +67,41 @@ def test_upload_project(client):
     assert response.status_code == 303
 
 
-def test_match_teams(client):
+def test_list_proj(client,test_db):
+    
+    expected_output=[
+        {
+        "name" : "Project_1",
+        "description" : "We are looking for a developer with strong skills in Python, Java, and SQL. Experience with Linux and Git is a plus",
+        "skills" : 'Python, Java, SQL, Linux, Git'
+        }
+    ]
+
+    response = client.get("/projects")
+
+    assert response.context["projects"] == expected_output
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == 'text/html; charset=utf-8'
+
+
+def test_match(client, test_db):
+    response = client.get('/match')
+
+    assert response.context["projects"] == [{'name': 'Project_1'}] 
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == 'text/html; charset=utf-8'
+
+
+def test_match_teams(client,test_db):
     data={
-        "name" : "dummy project",
-        "number" : 2
+        "name" : "Project_1",
+        "number" : 1
     }
     response = client.post("/matchs",
                             data = data
                         )
 
-    
+    assert response.context['teams'] == [{'name': 'dummy name', 'num_skills': 3, 'proj_name': 'Project_1'}]
     assert response.status_code == 200
     assert response.headers["Content-Type"] == 'text/html; charset=utf-8'
 
